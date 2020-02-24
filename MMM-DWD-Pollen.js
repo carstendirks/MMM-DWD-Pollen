3423d56a41d9f0bf7852124049de167b4d4e124c
Carsten Dirks <carsten.dirks@gmail.com>
2020-02-24 21:00:14 +0100

    changed operators

--- MMM-DWD-Pollen.js
+++ MMM-DWD-Pollen.js
@@ -126,43 +126,44 @@ Module.register("MMM-DWD-Pollen", {
 				
 				var pollenDataAvailable = 0;
 				
+
 				// Erle
-				if ((r.Pollen.Erle.today > 0) || (r.Pollen.Erle.tomorrow > 0)) {
+				if ((r.Pollen.Erle.today != 0) || (r.Pollen.Erle.tomorrow != 0)) {
 					writePollen(tbl, "Erle", r.Pollen.Erle.today, r.Pollen.Erle.tomorrow);
                 			pollenDataAvailable = 1;
 				};
 				// Roggen
-				if ((r.Pollen.Roggen.today > 0) || (r.Pollen.Roggen.tomorrow > 0)) {
+				if ((r.Pollen.Roggen.today != 0) || (r.Pollen.Roggen.tomorrow != 0)) {
 					writePollen(tbl, "Roggen", r.Pollen.Roggen.today, r.Pollen.Roggen.tomorrow);
                 			pollenDataAvailable = 1;
 				};
 				// Ambrosia
-				if ((r.Pollen.Ambrosia.today > 0) || (r.Pollen.Ambrosia.tomorrow > 0)) {
+				if ((r.Pollen.Ambrosia.today != 0) || (r.Pollen.Ambrosia.tomorrow != 0)) {
 					writePollen(tbl, "Ambrosia", r.Pollen.Ambrosia.today, r.Pollen.Ambrosia.tomorrow);
                 			pollenDataAvailable = 1;
 				};
 				// Esche
-				if ((r.Pollen.Esche.today > 0) || (r.Pollen.Esche.tomorrow > 0)) {
+				if ((r.Pollen.Esche.today != 0) || (r.Pollen.Esche.tomorrow != 0)) {
 					writePollen(tbl, "Esche", r.Pollen.Esche.today, r.Pollen.Esche.tomorrow);
-                			pollenDataAvailable = 1;
+                			pollenDataAvailable = 1;	
 				};
 				// Gräßer
-				if ((r.Pollen.Graeser.today > 0) || (r.Pollen.Graeser.tomorrow > 0)) {
+				if ((r.Pollen.Graeser.today != 0) || (r.Pollen.Graeser.tomorrow != 0)) {
 					writePollen(tbl, "Graeser", r.Pollen.Graeser.today, r.Pollen.Graeser.tomorrow);
                 			pollenDataAvailable = 1;
 				};
 				// Hasel
-				if ((r.Pollen.Hasel.today > 0) || (r.Pollen.Hasel.tomorrow > 0)) {
+				if ((r.Pollen.Hasel.today != 0) || (r.Pollen.Hasel.tomorrow != 0)) {
 					writePollen(tbl, "Hasel", r.Pollen.Hasel.today, r.Pollen.Hasel.tomorrow);
                 			pollenDataAvailable = 1;
 				};
 				// Birke
-				if ((r.Pollen.Birke.today > 0) || (r.Pollen.Birke.tomorrow > 0)) {
+				if ((r.Pollen.Birke.today != 0) || (r.Pollen.Birke.tomorrow != 0)) {
 					writePollen(tbl, "Birke", r.Pollen.Birke.today, r.Pollen.Birke.tomorrow);
                 			pollenDataAvailable = 1;
 				};
 				// Beifuss
-				if ((r.Pollen.Beifuss.today > 0) || (r.Pollen.Beifuss.tomorrow > 0)) {
+				if ((r.Pollen.Beifuss.today != 0) || (r.Pollen.Beifuss.tomorrow != 0)) {
 					writePollen(tbl, "Beifuss", r.Pollen.Beifuss.today, r.Pollen.Beifuss.tomorrow);
                 			pollenDataAvailable = 1;
 				};
